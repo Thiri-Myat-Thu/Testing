239f97f22021156edde522a5756e35e36049f931
Thiri-Myat-Thu <thirimyatthu.y2k3@gmail.com>
2025-01-03 10:48:21 +0630

    Add files via upload
    add

---
+++ src/components/Intro1.js
@@ -0,0 +1,16 @@
+function Intro1(){
+    return(
+        <div className = "blog-post-intro">
+            <h2>I've become a React Developer!</h2>
+
+            <div>
+                <p>I've complete the Reaact Basic courese and I've happy to announce that I'am now a junior Developer!</p>
+                <p className = "link">Read more...</p>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Intro;
---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo(){
+    return(
+        <div className = "promo-section">
+            <div>
+                <h1>Don't miss this deal!</h1>
+            </div>
+            <div>
+                <h2>Subscribe t my newsletter and get all the shp al the dhip items at 50% off!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
